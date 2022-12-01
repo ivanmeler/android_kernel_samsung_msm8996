156e5145a58ef76a990ed45b940118fc7fef830b
ivanmeler <i_ivan@windowslive.com>
2022-12-01 13:43:09 +0000

    build: strip out unneeded flags
    these ar ent mentioned anywhere in the source

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,7 @@ export CROSS_COMPILE=/home/ivanmeler/android/system/prebuilts/gcc/linux-x86/aarc
 
 mkdir out
 export ARCH=arm64
-export SEC_BUILD_OPTION_HW_REVISION=02
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android heroqlte_chn_open_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android -j8
+make -C $(pwd) O=$(pwd)/out heroqlte_chn_open_defconfig
+make -C $(pwd) O=$(pwd)/out -j8
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
